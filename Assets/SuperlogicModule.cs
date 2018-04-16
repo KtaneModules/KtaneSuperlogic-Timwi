60174905d119f8a98cd9c93cdb66cbd9402de9b7
Timwi <github@timwi.de>
2018-04-16 17:03:18 +0200

    Initial commit

---
+++ Assets/SuperlogicModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Superlogic;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Superlogic
+/// Created by Timwi
+/// </summary>
+public class SuperlogicModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
