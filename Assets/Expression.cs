5d7366310b5f805f6467d14e90c521f2d5f7c84c
Timwi <github@timwi.de>
2018-04-17 13:51:38 +0200

    Models, and some code

---
+++ Assets/Expression.cs
@@ -0,0 +1,36 @@
+﻿namespace Superlogic
+{
+    abstract class Expression { }
+
+    sealed class VariableExpression : Expression
+    {
+        public int Variable { get; private set; }
+        public VariableExpression(int variable) { Variable = variable; }
+    }
+
+    sealed class BinaryOperatorExpression : Expression
+    {
+        public Expression Left { get; private set; }
+        public Expression Right { get; private set; }
+        public BinaryOperator Operator { get; private set; }
+        public BinaryOperatorExpression(Expression left, Expression right, BinaryOperator op)
+        {
+            Left = left;
+            Right = right;
+            Operator = op;
+        }
+    }
+
+    enum BinaryOperator
+    {
+        And = 0,
+        Or = 1,
+        Xor = 2,
+        Nand = 3,
+        Nor = 4,
+        Xnor = 5,
+        Implies = 6,
+        ImpliedBy = 7,
+        NumOperators = 8
+    }
+}
--- Assets/SuperlogicModule.cs
+++ Assets/SuperlogicModule.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using Superlogic;
 using UnityEngine;
@@ -21,5 +20,25 @@ public class SuperlogicModule : MonoBehaviour
     void Start()
     {
         _moduleId = _moduleIdCounter++;
+
+        var numVariables = 3;
+        var variables = Enumerable.Range(0, numVariables).Select(x => (Expression) new VariableExpression(x)).ToList().Shuffle();
+        if (Rnd.Range(0, 10) == 0)
+            variables.RemoveAt(0);
+
+        while (variables.Count > 1)
+        {
+            var l = pickRandomAndRemove(variables);
+            var r = pickRandomAndRemove(variables);
+            variables.Add(new BinaryOperatorExpression(l, r, (BinaryOperator) Rnd.Range(0, (int) BinaryOperator.NumOperators)));
+        }
+    }
+
+    private T pickRandomAndRemove<T>(List<T> list)
+    {
+        var ix = Rnd.Range(0, list.Count);
+        var t = list[ix];
+        list.RemoveAt(ix);
+        return t;
     }
 }
